a960a85f69117dfad17f242e6186696caae06cb2
unknown <PS20ATello@efcts.us>
2019-10-15 14:06:45 -0400

    added product schema

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mydb');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+  Name:{type:String, required:true},
+  Picture:String,
+  Price:Number,
+  QTY:Number,
+  Description:String
+},{collection:'Product'});
+var Product = mongoose.model('Product',ProductSchema)
+
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
