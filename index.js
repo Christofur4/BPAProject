5f357bcef6de3d181454fa7c743e2c975c28ddc7
M Buziewicz <mbuziewicz@efcts.us>
2019-10-15 12:42:53 -0400

    myscripts.js added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var tools = require('/public/myscripts');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
