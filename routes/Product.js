8d7dd9e7fe8299acc3a063ecf294da723268fb09
unknown <PS20MPoudel@efcts.us>
2019-11-12 14:18:31 -0500

    saving project

--- routes/Product.js
+++ routes/Product.js
@@ -16,4 +16,18 @@ router.get('/', function(req, res, next) {
     });
   });
 
+  router.get('/product', function(req, res, next) {
+    //var successMsg=req.flash('success')[0];
+    Product.find(function(err, products){
+      console.log("I am here")
+    /*  var Numberatatime=[];
+      var Size=3;
+      for(var i=0;i<docs.length;i+=Size){
+        Numberatatime.push(docs.slice(i,i + Size));
+      }
+    */
+   res.render('pages/AllenDbProductPage', { title: 'Shopping Cart', products: products });
+    });
+  });
+
   module.exports = router;
