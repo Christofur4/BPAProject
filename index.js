42cea80317dc4fb512bac7f4c774837c686d6148
unknown <PS20MPoudel@efcts.us>
2019-11-19 12:23:54 -0500

    save

--- index.js
+++ index.js
@@ -20,13 +20,3 @@ express()
   .use('/', indexRouter)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-  
