8862cf1759372e4d15073da66c78917e34e7550c
unknown <PS20MPoudel@efcts.us>
2019-12-12 13:13:17 -0500

    meera's changes

--- routes/Product.js
+++ routes/Product.js
@@ -60,4 +60,18 @@ router.get('/', function(req, res, next) {
     });
   });
 
+  router.get('/signup', function(req, res, next) {
+    //var successMsg=req.flash('success')[0];
+    Product.find(function(err, products){
+      console.log("I am here")
+      var Numberatatime=[];
+     // var Size=3;
+     // for(var i=0;i<docs.length;i+=Size){
+     //   Numberatatime.push(docs.slice(i,i + Size));
+     // }
+    
+   res.render('pages/SignUpPage', { title: 'Shopping Cart', products: products });
+    });
+  }); 
+
   module.exports = router;
