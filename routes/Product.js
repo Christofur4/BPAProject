e46df6f7c1bcf288564cc4fe07becad6578a0b9d
unknown <PS20ATello@efcts.us>
2019-12-17 12:08:09 -0500

    allen's changes

--- routes/Product.js
+++ routes/Product.js
@@ -45,5 +45,23 @@ router.get('/', function(req, res, next) {
     });
   });
 
+  
+  router.get('/modal', function(req, res, next) {
+    //var successMsg=req.flash('success')[0];
+    Product.find(function(err, products){
+      console.log("I am here")
+    /*  var Numberatatime=[];
+      var Size=3;
+      for(var i=0;i<docs.length;i+=Size){
+        Numberatatime.push(docs.slice(i,i + Size));
+      }
+    */
+   res.render('pages/modal', { title: 'Shopping Cart', products: products });
+    });
+  });
+
+
+
+
   module.exports = router;
 
